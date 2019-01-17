419161449ec67dece30b136064d20da21a605f0c
PetarPeychev <petarpeychev98@gmail.com>
2019-01-17 20:23:00 +0000

    command-line argument test

--- interpreter/lexer.cpp
+++ interpreter/lexer.cpp
@@ -1,14 +1,26 @@
 #include <string>
+#include <cstring>
 
 using std::string;
 
 int tokenize(string source) {
 
-
   return 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+
+  if (argc == 1)
+    printf("Lexical Analysis Error: No file name argument provided!");
+
+  if (argc > 2)
+    printf("Lexical Analysis Error: Too many arguments provided");
+
+  if (argc >= 2) {
+    printf("\n----Following Are The Command Line Arguments Passed----");
+    for(int i = 0; i < argc; i++)
+      printf("\nargv[%d]: %s",i,argv[i]);
+  }
 
   tokenize("increment x = x + 1");
 
