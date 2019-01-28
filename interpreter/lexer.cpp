da6acb07dbbd7a4478103e8e0b7b8651d4f98dfb
PetarPeychev <petarpeychev98@gmail.com>
2019-01-28 16:16:56 +0000

    added multi-char and single-char symbol parsing

--- interpreter/lexer.cpp
+++ interpreter/lexer.cpp
@@ -35,7 +35,38 @@ std::vector<Lexeme> tokenize(std::string source) {
       continue;
     }
 
-    std::regex single_op("[+\\-\\*/]");
+    std::regex single_symbol("[:]");
+    if (regex_match(c, single_symbol)) {
+      Lexeme single_symbol = { SYMBOL, c};
+      lexemes.push_back(single_symbol);
+      current++;
+      std::cout << "Single symbol: " << c << std::endl; // Remove in production
+      continue;
+    }
+
+    std::regex potential_double_s("[|]");
+    if (regex_match(c, potential_double_s)) {
+      if (current + 1 < int(max_length)) {
+        std::string c_new = c + source[current + 1];
+
+        std::regex double_s("|>");
+        if (regex_match(c_new, double_s)) {
+          Lexeme double_symbol = { SYMBOL, c_new };
+          lexemes.push_back(double_symbol);
+          current += 2;
+          std::cout << "Double symbol: " << c_new << std::endl; // Remove in production
+          continue;
+        }
+      }
+
+      Lexeme single_symbol = { SYMBOL, c };
+      lexemes.push_back(single_symbol);
+      current++;
+      std::cout << "Single symbol: " << c << std::endl; // Remove in production
+      continue;
+    }
+
+    std::regex single_op("[+\\-\\*/=]");
     if (regex_match(c, single_op)) {
       Lexeme single_operator = { OPERATOR, c};
       lexemes.push_back(single_operator);
