b13f1e14ddb7f818c7cca70e06b0a7dd409c3cab
PetarPeychev <petarpeychev98@gmail.com>
2019-02-05 23:11:31 +0000

    added placeholder preprocess code

--- interpreter/preprocessor.cpp
+++ interpreter/preprocessor.cpp
@@ -1,5 +1,24 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include "preprocessor.h"
 
 std::vector<Lexeme> preprocess(std::vector<Lexeme> tokens) {
+
+  // Find import statements
+  for (unsigned int i = 0; i < tokens.size(); i++) {
+    if (tokens[i].value == "import") {
+        if (tokens[i].type == NAME) {
+
+          // TODO: Implement import statements
+
+        }
+    }
+
+    std::cout << tokens[i].value << " "; // Remove in production
+  }
+
+  std::cout << std::endl; // Remove in production
+
   return tokens;
-};
+}
